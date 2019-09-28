41fd1739ab3d26716d5c389d573f597aa084d331
ritik-gupta <ritikg8126@gmail.com>
2019-09-28 11:24:01 +0530

    .f

--- userbot/modules/f.py
+++ userbot/modules/f.py
@@ -3,13 +3,17 @@ import html
 from userbot import HELPER, bot
 from userbot.events import register
 
-@register(outgoing=True, pattern=r"^.f$")
-async def fcmd(message):
-    n = message.text[-1]
-    if message.text[-1] == 'f' and message.text[0]=='.':
-        await message.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
+@register(outgoing=True, pattern=r"^.f")
+async def fcmd(e):
+    message = e.text
+    if message == ".fp" and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("🤦‍♂")
+
+    elif message == '.f' and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
     
-    elif message.text[0]=='.':
+    elif message.text[0]=='.' and e.text[0] not in ("/", "#", "@", "!"):
+        n= message[-1]
         out = ""
         for line in [5, 1, 1, 4, 1, 1, 1]:
             c = max(line, 1)

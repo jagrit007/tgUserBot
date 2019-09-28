b86cc757682b921b487dd8609ffec92bf1fd924f
jagrit007 <40325232+jagrit007@users.noreply.github.com>
2019-09-29 01:00:47 +0530

    f

--- string_session.py
+++ string_session.py
@@ -1,4 +1,3 @@
-import socks
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 from userbot import API_KEY, API_HASH
@@ -8,6 +7,5 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
 
-proxy=(socks.SOCKS5, 'proxy.server', 3128)
-with TelegramClient(StringSession(), API_KEY, API_HASH, proxy=proxy) as client:
+with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print(client.session.save())

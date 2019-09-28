cc5a89db7160633787c2dbd825b0875efc1130fa
ritik-gupta <ritikg8126@gmail.com>
2019-09-28 12:14:06 +0530

    added .solar

--- userbot/modules/f.py
+++ userbot/modules/f.py
@@ -5,20 +5,21 @@ from userbot.events import register
 
 @register(outgoing=True, pattern=r"^.f")
 async def fcmd(e):
-    message = e.text
-    if message == ".fp" and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("🤦‍♂")
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
 
-    elif message == '.f' and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
+        if message[-1] == 'p' and message[-2] == 'f':
+            await e.edit("🤦‍♂")
+        elif message[-1] == 'f':
+            await e.edit("┏━━━┓\n┃┏━━┛\n┃┗━━┓\n┃┏━━┛\n┃┃\n┗┛")
     
-    elif message.text[0]=='.' and e.text[0] not in ("/", "#", "@", "!"):
-        n= message[-1]
-        out = ""
-        for line in [5, 1, 1, 4, 1, 1, 1]:
-            c = max(line, 1)
-            out += (n * c) + "\n"
-        await message.edit(html.escape(out))
+        else:
+            n= message[-1]
+            out = ""
+            for line in [5, 1, 1, 4, 1, 1, 1]:
+                c = max(line, 1)
+                out += (n * c) + "\n"
+            await message.edit(html.escape(out))
 
 
 
